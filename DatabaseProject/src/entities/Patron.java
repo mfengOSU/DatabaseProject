9dfae2903715a6f350daf498079afa2927435412
Yijia-Wang <63363673+Yijia-Wang@users.noreply.github.com>
2022-03-01 19:32:46 -0500

    Update Patron.java
    add main

--- DatabaseProject/src/entities/Patron.java
+++ DatabaseProject/src/entities/Patron.java
@@ -1,5 +1,15 @@
 package entities;
-
+import java.util.Scanner;
 public class Patron {
-	
+	System.out.println("Please enter the patron ID:");
+	Scanner input=new Scanner(System.in);
+	String ID=input.nextLine();
+	System.out.println("Please enter the patron first name:");
+	String first=input.nextLine();
+	System.out.println("Please enter the patron last name:");
+	String last=input.nextLine();
+	System.out.println("Please enter the patron address:");
+	String add=input.nextLine();
+	System.out.println("Please enter the patron email:");
+	String email=input.nextLine();
 }
