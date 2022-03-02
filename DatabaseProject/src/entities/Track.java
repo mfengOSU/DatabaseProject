e45457f720bc6b6cf78881c67f0bade5597612ec
Yijia-Wang <63363673+Yijia-Wang@users.noreply.github.com>
2022-03-01 19:36:21 -0500

    Update Track.java
    add

--- DatabaseProject/src/entities/Track.java
+++ DatabaseProject/src/entities/Track.java
@@ -1,5 +1,11 @@
 package entities;
-
+input java.util.Scanner;
 public class Track {
-
+System.out.println("Please enter the item number");
+Scanner input=new Scanner(System.in);
+  int num=input.nextInt();
+  System.out.println("Please enter the item title");
+String title=input.nextLine();
+  System.out.println("Please enter the item length");
+String len=input.nextLine();
 }
